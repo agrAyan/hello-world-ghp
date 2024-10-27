a14d63e407e757c8c09d60fb381a58ac67d9b421
Ayan Agrawal <aagrawal@scan-it.com.sg>
2024-10-27 11:31:26 +0530

    Auto-generated commit

--- server/chunk-NIZOLU6Q.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{A as g,a as p,b as r,c as n,d as i,e as m,f as a,k as f,n as s,p as c,q as l,t as d,x as u}from"./chunk-4CZUMJSL.mjs";var o=class t{title="hello-world-ghp";static \u0275fac=function(e){return new(e||t)};static \u0275cmp=p({type:t,selectors:[["app-root"]],standalone:!0,features:[a],decls:3,vars:0,template:function(e,b){e&1&&(r(0,"h1"),m(1,"Welcome "),n(),i(2,"router-outlet"))},dependencies:[u]})};var C=[];var v={providers:[f({eventCoalescing:!0}),g(C),l()]};var h={providers:[d()]},A=s(v,h);var R=()=>c(o,A),B=R;export{B as a};
--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-NIZOLU6Q.mjs";import"./chunk-4CZUMJSL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
